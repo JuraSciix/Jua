d695226574cfe755419caa4d91aad4b080a8f768
JuraSciix <jurasciix@gmail.com>
2022-12-11 04:15:03 +0300

    Fix: fixes -l (lint) option

--- jua/src/jua/compiler/JuaCompiler.java
+++ jua/src/jua/compiler/JuaCompiler.java
@@ -1,6 +1,9 @@
 package jua.compiler;
 
+import jua.compiler.Tokens.Token;
+import jua.compiler.Tokens.TokenType;
 import jua.util.IOUtils;
+import jua.util.Options;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +20,34 @@ public final class JuaCompiler {
             return null;
         }
         Source source = new Source(file.getName(), filecontents);
+
+        if (Options.lint()) {
+            Lexer tokenizer = new Tokenizer(source);
+            for (Token token = tokenizer.nextToken();
+                 token.type != TokenType.EOF;
+                 token = tokenizer.nextToken()) {
+                System.out.printf("[%d:%d] ",
+                        source.getLineMap().getLineNumber(token.pos),
+                        source.getLineMap().getColumnNumber(token.pos));
+                switch (token.type.kind) {
+                    case DEFAULT:
+                        System.out.println(token.type.name());
+                        break;
+                    case NAMED:
+                        System.out.printf("%s: %s%n", token.type.name(), token.name());
+                        break;
+                    case STRING:
+                        System.out.printf("%s: \"%s\"%n", token.type.name(), token.value());
+                        break;
+                    case NUMERIC:
+                        System.out.printf("%s: %s * %d%n", token.type.name(), token.value(), token.radix());
+                        break;
+                    default:
+                        throw new AssertionError(token.type);
+                }
+            }
+            return null;
+        }
         try {
             ProgramLayout programLayout = new ProgramLayout();
 
