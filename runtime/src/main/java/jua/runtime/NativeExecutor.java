78b3448231eb4886d671d30d5b63a97c2d46892c
JuraSciix <jurasciix@gmail.com>
2024-03-13 18:57:04 +0300

    Рефакторинг (runtime): помечает jua.runtime.NativeExecutor за deprecated

--- runtime/src/main/java/jua/runtime/NativeExecutor.java
+++ runtime/src/main/java/jua/runtime/NativeExecutor.java
@@ -2,6 +2,9 @@ package jua.runtime;
 
 import jua.runtime.interpreter.Address;
 
+/**
+ * @deprecated Скоро будет заменено на {@link JuaCallable}.
+ */
 @FunctionalInterface
 public interface NativeExecutor {
 
