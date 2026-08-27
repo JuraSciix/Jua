2eb0ce8f0535c5c728a10b373ef8db51ad1dc8e8
JuraSciix <jurasciix@gmail.com>
2026-08-27 13:30:37 +0300

    runtime: добавляет метод `toString`

--- runtime/src/main/java/jua/vm/SimpleArena.java
+++ runtime/src/main/java/jua/vm/SimpleArena.java
@@ -2,6 +2,8 @@ package jua.vm;
 
 import jua.vm.arena.DataArena;
 
+import java.util.Objects;
+
 import static jua.vm.SimpleType.*;
 
 public class SimpleArena {
@@ -24,5 +26,16 @@ public class SimpleArena {
     public static void clearAndMove(DataArena arena, int src, int dst) {
         arena.clear(dst);
         arena.move(src, dst);
+    }
+
+    public static String toString(DataArena arena, int addr) {
+        byte type = arena.readType(addr);
+        switch (type) {
+            case TYPE_INT64: return Long.toString(arena.readInt64(addr));
+            case TYPE_FLOAT64: return Double.toString(arena.readFloat64(addr));
+            case TYPE_BOOL64: return Boolean.toString(arena.readBool(addr));
+            case TYPE_REF: return Objects.toString(arena.readBool(addr));
+            default: return String.format("%02x:%016x", type, arena.readInt64(addr));
+        }
     }
 }
